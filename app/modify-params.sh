041689196b1d9695c24f7d75240d1478f0079684
Vadim Tikhanoff <vtikha@gmail.com>
2021-01-21 15:47:00 +0100

    Update modify-params.sh

--- app/modify-params.sh
+++ app/modify-params.sh
@@ -4,11 +4,10 @@ function copyParams () {
     local file=$1
     local -n icubEyesElem=$2
     local -n outputElem=$3
-    
-    for i in {0..2}
+
+    for i in $( seq 0 $sizeOfElements )
     do
         index=3
-        echo "Running for ${icubEyesElem[$i,0]} ..."
         for (( c=${icubEyesElem[$i,1]}+1; c<=${icubEyesElem[$((i+1)),1]}-1; c++ ))
         do
             tmp="$(sed $c!d $file)"
@@ -24,6 +23,30 @@ function copyParams () {
             fi
         done
     done 
+
+    if [ $sizeOfElements = 1 ]
+    then
+        c=$(( icubEyesElem[2,1] ))
+        NumLines=$((outputElem[3,1]-outputElem[2,1]))
+
+        echo "" >> $icubEyesFile
+    
+        index=2
+
+        for i in $( seq 0 $((NumLines-1)) )
+        do    
+            if [ $i = 1 ]
+            then
+                sed "${c}s/.*/[${outputElem[2,0]}]/" $icubEyesFile  > tmp.txt
+                mv tmp.txt $icubEyesFile
+            else
+                echo "" >> $icubEyesFile
+                sed "$((c+$i))s/.*/${outputElem[2,$index]}/" $icubEyesFile  > tmp.txt
+                mv tmp.txt $icubEyesFile
+                index=$((index+1))
+            fi
+        done
+    fi
 }
 
 function getLine () {
@@ -37,6 +60,11 @@ function getFileParameters () {
 
     local file=$1
     local -n arr=$2
+    
+    #add two end lines
+    echo "" >> $file
+    echo "" >> $file
+
     local endLine="$(wc -l $file | awk '{ print $1 }' )"
     
     arr[0,0]="CAMERA_CALIBRATION_RIGHT"
@@ -45,10 +73,21 @@ function getFileParameters () {
     arr[1,1]=$(getLine "${arr[1,0]}" "$file")
     arr[2,0]="STEREO_DISPARITY"
     arr[2,1]=$(getLine "${arr[2,0]}" "$file")
-    arr[3,0]="ENDOFFILE"
-    arr[3,1]=$((endLine+1))
 
-    for i in {0..2}
+    if [ -z "${arr[2,1]}" ]
+    then
+        echo "STEREO_DISPARITY not available, will create it"
+        arr[2,0]="ENDOFFILE"
+        arr[2,1]=$((endLine+1))
+    else
+        arr[3,0]="ENDOFFILE"
+        arr[3,1]=$((endLine+1))
+    fi
+
+    tmp=${#arr[@]}
+    sizeOfElements=$((tmp / 4))
+
+    for i in $( seq 0 $sizeOfElements )
     do 
         index=3
         for (( c=${arr[$i,1]}+1; c<=${arr[$((i+1)),1]}-1; c++ ))
@@ -100,7 +139,7 @@ do
   sleep 2 # or less like 0.2
   echo "waiting for file ..."
 done
-ls -l $outputFile
+#ls -l $outputFile
 
 echo "Got the file waiting for safety..."
 sleep 5
@@ -112,6 +151,11 @@ getFileParameters $icubEyesFile icubEyesElements
 #echo "Got ${outputElements[1,0]} ${outputElements[1,1]} ${outputElements[1,2]} ${outputElements[1,5]} ${outputElements[1,6]}"
 
 copyParams $icubEyesFile icubEyesElements outputElements 
+
+#clean up files
+sed -i '$ d' $outputFile
+#sed -i '$ d' $icubEyesFile
+#sed -i '' -e '$ d' $outputFile
 
 camCalib --name /camCalib/right --from $icubEyesFile --group CAMERA_CALIBRATION_RIGHT > /dev/null 2>&1 &
 camCalib --name /camCalib/left --from $icubEyesFile --group CAMERA_CALIBRATION_LEFT > /dev/null 2>&1
