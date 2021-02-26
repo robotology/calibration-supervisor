02ff01ab3ca7f219802c7bdeade264f27a69d651
Valentina Vasco <valentina.vasco@iit.it>
2021-02-26 15:30:03 +0100

    added event-cameras folder with conf files and apps

--- app/create-calibration-test-set.sh
+++ app/create-calibration-test-set.sh
@@ -3,17 +3,21 @@
 # to have dot separated values instead of comma separated values
 #LANG=en_US
 
+# create-calibration-test-set.sh run camera-calibration-best-pos/event-cameras 1 100
+
 # launch the demo
 run() {
-    TEST_SET=$1
-    NIMAGES=$2
+    CONTEXT=$1
+    TEST_SET=$2
+    NIMAGES=$3
+    echo "Running for context $CONTEXT" 
     yarpserver --write --silent &
 
-    gazebo camera-calibration-chessboard-closer.sdf &
+    gazebo $CONTEXT/camera-calibration-chessboard-closer.sdf &
 
     yarp wait /icubSim/head/state:o
     yarp wait /icubSim/inertial
-    iKinGazeCtrl --context camera-calibration-best-pos --from iKinGazeCtrl.ini --torso off &
+    iKinGazeCtrl --context $CONTEXT --from iKinGazeCtrl.ini --torso off &
  
     yarp wait /chessboard/mover:i
     yarp wait /iKinGazeCtrl/x:o
@@ -21,12 +25,12 @@ run() {
     yarp wait /iKinGazeCtrl/q:o
     yarp wait /iKinGazeCtrl/events:o
 
-    movePattern --period 0.1 --save false --create_test true --nimages $NIMAGES --maxx 0.0 --maxy 0.0 &
+    movePattern --context $CONTEXT --period 0.1 --save false --create_test true --nimages $NIMAGES --maxx 0.0 --maxy 0.0 &
 
     yarp wait /movePattern/gt:o
     yarp wait /movePattern/distorted:o   
 
-    yarpmanager-console --application ${ICUBcontrib_DIR}/share/ICUBcontrib/applications/camera-calibration/camera-calibration-gazebo-test-img-app.xml --run --connect --exit --silent
+    yarpmanager-console --application ${ICUBcontrib_DIR}/share/ICUBcontrib/applications/camera-calibration-supervisor/$CONTEXT/camera-calibration-gazebo-test-img-app.xml --run --connect --exit --silent
 
     echo "Start!"
     for i in $( eval echo {1..$TEST_SET} )
@@ -76,7 +80,7 @@ clean() {
 echo "********************************************************************************"
 echo ""
 
-$1 $2 $3
+$1 $2 $3 $4
 
 if [[ $# -eq 0 ]] ; then
     echo "No options were passed!"
--- app/evaluate-calibration.sh
+++ app/evaluate-calibration.sh
@@ -3,11 +3,13 @@
 # to have dot separated values instead of comma separated values
 #LANG=en_US
 
-FOLDER=$ROBOT_CODE"/camera-calibration/datasets/testSet"
+# evaluate-calibration.sh run camera-calibration-best-pos/event-cameras /home/vvasco/dev/robotology/camera-calibration-supervisor/testsets/event-cameras 200
 
 # launch the demo
 run() {
-    CANDIDATES=$1
+    CONTEXT=$1
+    FOLDER=$2
+    CANDIDATES=$3
     yarpserver --write --silent &
 
     yarpdataplayer &
@@ -20,31 +22,41 @@ run() {
     calibEvaluator &    
     for i in $( eval echo {1..$CANDIDATES} )
     do
-      dstfile="/home/vvasco/.local/share/yarp/contexts/camera-calibration-best-pos/candidatePos_$i/score.txt"
-      camCalib --robotName icubSim --name /camCalib/left --context camera-calibration-best-pos/candidatePos_$i --from outputCalib.ini --group CAMERA_CALIBRATION_LEFT --CAMERA_CALIBRATION_LEFT::drawCenterCross 0 &
-      yarpmanager-console --application ${ICUBcontrib_DIR}/share/ICUBcontrib/applications/camera-calibration/evaluate-calibration.xml --run --connect --exit --silent
-      echo "play" | yarp rpc /yarpdataplayer/rpc:i    
-      echo "Start!"
-      while true; do
-        check=$(echo "getSliderPercentage" | yarp rpc /yarpdataplayer/rpc:i | awk '{print $2}')
-        if [ $check -eq "100" ]; then
-          echo "Done!"
-          echo "Saving score..."
-          score=$(echo "getScore" | yarp rpc /calibEvaluator/rpc | awk '{print $2}')
-          echo $score
-          echo $score >| $dstfile
+      dstfile="/home/vvasco/.local/share/yarp/contexts/$CONTEXT/candidatePos_$i/score.txt"
+      calibfile="/home/vvasco/.local/share/yarp/contexts/$CONTEXT/candidatePos_$i/outputCalib.ini"
+      echo $calibfile
+      if [ -s $calibfile ]; then
+          camCalib --robotName icubSim --name /camCalib/left --context $CONTEXT/candidatePos_$i --from outputCalib.ini --group CAMERA_CALIBRATION_LEFT --CAMERA_CALIBRATION_LEFT::drawCenterCross 0 &
+      yarpmanager-console --application ${ICUBcontrib_DIR}/share/ICUBcontrib/applications/camera-calibration-supervisor/$CONTEXT/evaluate-calibration.xml --run --connect --exit --silent
+          echo "play" | yarp rpc /yarpdataplayer/rpc:i    
+          echo "Start!"
+          while true; do
+              check=$(echo "getSliderPercentage" | yarp rpc /yarpdataplayer/rpc:i | awk '{print $2}')
+              if [ $check -eq "100" ]; then
+                  echo "Done!"
+                  echo "Saving score..."
+                  score=$(echo "getScore" | yarp rpc /calibEvaluator/rpc | awk '{print $2}')
+                  echo $score
+                  echo $score >| $dstfile
+                  sleep 1
+                  echo "Saved score!"
+                  echo "reset" | yarp rpc /calibEvaluator/rpc
+                  break
+              fi
+          done
+          echo "Done loop!"   
           sleep 1
-          echo "reset" | yarp rpc /calibEvaluator/rpc
-          break
-        fi
-      done
-      sleep 1
-      echo "Closing camCalib"
-      killall camCalib
-      sleep 1
-    done 
-
-    sleep 1
+          echo "Closing camCalib"
+          killall camCalib
+          sleep 1
+      else
+        echo "outputCalib is empty"
+        echo "Skipping"  
+        echo 0.0 >| $dstfile
+      fi
+   done 
+ 
+   sleep 1
     declare -a modules=("yarpdataplayer" "calibEvaluator" "yarpview")
     for module in ${modules[@]}; do
         killall ${module}
@@ -72,7 +84,7 @@ clean() {
 echo "********************************************************************************"
 echo ""
 
-$1 $2 $3
+$1 $2 $3 $4 $5
 
 if [[ $# -eq 0 ]] ; then
     echo "No options were passed!"
