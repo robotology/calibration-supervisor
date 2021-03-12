e048c5ba7d19b615a8f3f09fac8d04148092ba1f
Valentina Vasco <valentina.vasco@iit.it>
2021-03-12 09:54:04 +0100

    [run-all.sh] fixed mono calib

--- app/run-all.sh
+++ app/run-all.sh
@@ -159,7 +159,7 @@ declare -A outputElements
 declare -A icubEyesElements
 
 echo "Running stereoCalib with monoCalib $mono"
-stereoCalib --robotName $robotName --context $calibContext --from $icubEyesFile --STEREO_CALIBRATION_CONFIGURATION::numberOfPairs 30 --STEREO_CALIBRATION_CONFIGURATION::monoCalib $mono & 
+stereoCalib --robotName $robotName --context $calibContext --from $icubEyesFile --STEREO_CALIBRATION_CONFIGURATION::numberOfPairs 30 --STEREO_CALIBRATION_CONFIGURATION::MonoCalib $mono & 
 
 FILE=$outputFile
 while [ ! -f $outputFile ]
